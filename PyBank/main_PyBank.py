be34f754345b1a0b9ddb8dd821afe70decfd6548
Hugh Kelly <hughjkelly@yahoo.com>
2023-02-12 13:16:24 -0500

    all good

---
+++ PyBank/main_PyBank.py
@@ -0,0 +1,20 @@
+#import dependencies
+import os
+import csv
+
+#assign csv data to variable
+open("Resources/budget_data.csv", "r")
+
+
+"""
+#read csv
+csv_reader = csv.reader(budget_data_csv, delimiter=",")
+
+#Read and print the header row
+csv_header = next(csv_reader)
+print(f"Header: {csv_header}")
+
+#Read and print all rows of csv data
+for row in budget_data_csv:
+    print(row)
+"""
